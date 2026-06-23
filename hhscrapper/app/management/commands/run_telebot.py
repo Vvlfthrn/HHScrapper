89ff3aa3536e42fe13b0e4c9073d89b229cb2ac9
vvlfthrn <Vvlfthrn@gmail.com>
2026-06-23 23:21:37 +1100

    added task hash to prevent repeated executions for duplicated vacancies

--- hhscrapper/app/management/commands/run_telebot.py
+++ hhscrapper/app/management/commands/run_telebot.py
@@ -31,26 +31,30 @@ def get_vacancy_description(vd: VacancyPromptDecision):
     return description
 
 async def send_messages(vacancy_decisions:Iterable[VacancyPromptDecision]):
-    bot = TelegramClient('./hhscrapper/sender_bot.db', API_ID, API_HASH)
-    client = await bot.start(bot_token=BOT_TOKEN)
-    vacancy_decisions = [x async for x in VacancyPromptDecision.objects.filter(
-        id__in=vacancy_decisions, koef__gte=models.F('prompt__koef')).select_related('vacancy', 'prompt')]
-    offset = 8
-    step = 0
-    while vacs:=vacancy_decisions[offset * step:offset * (step + 1)]:
-        message = ''
-        for vd in vacs:
-            message += get_vacancy_description(vd)
-        logger.info(f'Sending message to {USER_ID} vacs: {[x.id for x in vacs]}')
-        await client.send_message(USER_ID, message=message)
-        for vd in vacs:
-            vd.notified = True
-            await vd.asave(update_fields=['notified'])
-        logger.info(f'Sleeping {SLEEP_TIME} seconds')
-        await asyncio.sleep(SLEEP_TIME)
-        step += 1
-    await bot.disconnect()
-    return True
+    try:
+        bot = TelegramClient('./hhscrapper/sender_bot.db', API_ID, API_HASH)
+        client = await bot.start(bot_token=BOT_TOKEN)
+        vacancy_decisions = [x async for x in VacancyPromptDecision.objects.filter(
+            id__in=vacancy_decisions, koef__gte=models.F('prompt__koef')).select_related('vacancy', 'prompt')]
+        offset = 8
+        step = 0
+        while vacs:=vacancy_decisions[offset * step:offset * (step + 1)]:
+            message = ''
+            for vd in vacs:
+                message += get_vacancy_description(vd)
+            logger.info(f'Sending message to {USER_ID} vacs: {[x.id for x in vacs]}')
+            await client.send_message(USER_ID, message=message)
+            for vd in vacs:
+                vd.notified = True
+                await vd.asave(update_fields=['notified'])
+            logger.info(f'Sleeping {SLEEP_TIME} seconds')
+            await asyncio.sleep(SLEEP_TIME)
+            step += 1
+        await bot.disconnect()
+        return True
+    except ConnectionError as e:
+        logger.error(e)
+    return False
 
 
 def send_message_by_email(vacancy_decisions):
@@ -59,38 +63,39 @@ def send_message_by_email(vacancy_decisions):
 
     vacancies = VacancyPromptDecision.objects.filter(
         id__in=vacancy_decisions, koef__gte=models.F('prompt__koef')).select_related('vacancy', 'prompt')
-    message = ''
-    for vd in vacancies:
-        message += get_vacancy_description(vd)
+    if vacancies:
+        message = ''
+        for vd in vacancies:
+            message += get_vacancy_description(vd)
 
-    email = EmailMessage()
-    email['From'] = SENDER_EMAIL
-    email['To'] = RECEIVER_EMAIL
-    email['Subject'] = 'New vacancies'
-    email.set_content(message)
-    logger.info(f'Sending message to {USER_ID} vacs: {[x.id for x in vacancies]}')
+        email = EmailMessage()
+        email['From'] = SENDER_EMAIL
+        email['To'] = RECEIVER_EMAIL
+        email['Subject'] = 'New vacancies'
+        email.set_content(message)
+        logger.info(f'Sending message to {USER_ID} vacs: {[x.id for x in vacancies]}')
 
-    try:
-        context = ssl.create_default_context()
-        with smtplib.SMTP_SSL('smtp.gmail.com', 465, context=context) as server:
-            server.login(SENDER_EMAIL, APP_PASSWORD)
-            server.send_message(email)
-            logger.info(f'Send message successful')
-            for vd in vacancies:
-                vd.notified = True
-                vd.save(update_fields=['notified'])
-            return True
-    except smtplib.SMTPException as e:
-        logger.error(e)
-        return False
-
+        try:
+            context = ssl.create_default_context()
+            with smtplib.SMTP_SSL('smtp.gmail.com', 465, context=context) as server:
+                server.login(SENDER_EMAIL, APP_PASSWORD)
+                server.send_message(email)
+                logger.info(f'Send message successful')
+                for vd in vacancies:
+                    vd.notified = True
+                    vd.save(update_fields=['notified'])
+                return True
+        except smtplib.SMTPException as e:
+            logger.error(e)
+            return False
+    return True
 
 def bot_do_work():
     query = VacancyPromptDecision.objects.filter(state=DecisionState.CONSENSUS_CHECKED)
     if query.exists():
         vacancy_decisions = query.values_list('id', flat=True)
         if (
-                asyncio.run(send_messages(vacancy_decisions)) or
+                # asyncio.run(send_messages(vacancy_decisions)) or
                 send_message_by_email(vacancy_decisions)
         ):
             VacancyPromptDecision.objects.filter(id__in=vacancy_decisions).update(state=DecisionState.DONE)
